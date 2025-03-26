275a0ffa28ccac5cc4d12f4f4a5da86c31c77201
J00naTHan <jonathan.dutra.2004@gmail.com>
2025-03-26 04:05:19 -0300

    all shapes added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import consts as c
 from coord_plane import coord_plane
+from transformations import translation, scale, rotation
 
 if __name__ == "__main__":
     pygame.init()
@@ -19,11 +20,11 @@ if __name__ == "__main__":
 
         coord_plane(screen)
 
-        #triangle = pygame.draw.polygon(screen, c.RED, [(), (), ()])
-        #rectangle = pygame.draw.polygon(screen, c.GRAY, [(), (), (), ()])
-        #trapezium = pygame.draw.polygon(screen, c.RED, [(), (), (), ()])
-        #trect1 = pygame.draw.polygon(screen, c.BLUE, [(), (), ()])
-        #trect2 = pygame.draw.polygon(screen, c.BLUE, [(), (), ()])
+        triangle = pygame.draw.polygon(screen, c.RED, [(235, 469), (275, 519), (315, 469)])
+        rectangle = pygame.draw.polygon(screen, c.GRAY, [(346, 340), (346, 260), (546, 260), (546, 340)])
+        trapezium = pygame.draw.polygon(screen, c.RED, [(50, 50), (70, 70), (110, 70), (130, 50)])
+        trect1 = pygame.draw.polygon(screen, c.BLUE, [(786, 135), (736, 50), (736, 135)])
+        trect2 = pygame.draw.polygon(screen, c.BLUE, [(446, 578), (471, 578), (471, 578 - 42.5)])
         circle = pygame.draw.circle(screen, c.TURQUOISE, (57, 548), 30)
 
         pygame.display.flip()
